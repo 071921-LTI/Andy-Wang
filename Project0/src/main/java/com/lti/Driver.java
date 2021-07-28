8ecacab7005b4a17dd4877ce41bffbef0f6a245c
andyw177 <andyw177@gmail.com>
2021-07-28 13:10:49 -0400

    add db connection

--- Project0/src/main/java/com/lti/Driver.java
+++ Project0/src/main/java/com/lti/Driver.java
@@ -1,5 +1,9 @@
 package com.lti;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import com.lti.controllers.MenuScreen;
 import com.lti.daos.UserDao;
 import com.lti.daos.UserFile;
@@ -8,6 +12,15 @@ import com.lti.exceptions.UserNotFoundException;
 public class Driver {
 
 	public static void main(String[] args) {
+		String url = "jdbc:postgresql://localhost:5432/postgres";
+		String username = "postgres";
+		String password = "anw15102";
+		try(Connection con = DriverManager.getConnection(url,username,password)) {
+			System.out.println(con.getMetaData().getDriverName());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		MenuScreen.display();
 	}
 
--- Project0/src/main/java/com/lti/daos/SystemDao.java
+++ Project0/src/main/java/com/lti/daos/SystemDao.java
@@ -1,7 +1,11 @@
 package com.lti.daos;
 
+import java.util.List;
+
+import com.lti.models.Shoes;
+
 public interface SystemDao {
-	public abstract void displayItems();
+	public abstract List<Shoes> displayItems();
 	public abstract void setItemStatus(String item, String status);
 	public abstract String getItemStatus(String status);
 	public abstract double getWeeklyPayments();
